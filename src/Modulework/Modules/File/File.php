531d7dd6954c987b5b6c8bec92fa5fd22c83003e
ChristianGaertner <christiangaertner.film@googlemail.com>
2013-08-06 17:23:45 +0200

    Adding first stuff

--- src/Modulework/Modules/File/File.php
+++ src/Modulework/Modules/File/File.php
@@ -5,8 +5,53 @@
  * License: View distributed LICENSE file
  */
 
+use SplFileInfo;
+use Modulework\Modules\File\Exceptions\FileNotFoundException;
+
 /**
 * File
 * This class represents a file.
 */
-class File { }
+class File extends SplFileInfo
+{
+
+	/**
+	 * Constructor.
+	 * Create a new File instance for a file
+	 * @param string  $path   The file' s path
+	 * @param boolean $verify Whether to check for existens
+	 */
+	public function __construct($path, $verify = true)
+	{
+		if ($verify && !FileSystem::exists($path)) {
+			throw new FileNotFoundException($path);
+		}
+
+		parent::__construct($path);
+	}
+
+	public function getExtension()
+	{
+		return pathinfo($this->getBasename(), PATHINFO_EXTENSION);
+	}
+
+	public function move($targetDir, $targetName = null, $mode = 0666)
+	{
+		$target = $this->getTargetFile($targetDir, $targetName);
+
+		FileSystem::move($this->getPathname(), $target);
+
+		// Fix perms
+		FileSystem::chmod($target, $mode);
+
+		return $target;
+	}
+
+	protected function getTargetFile($dir, $name = null, $mode = 0666)
+	{
+		if (!FileSystem::isDirectory($dir)) {
+			FileSystem::makeDirectory($dir, $mode, true);
+		}
+	}
+
+}
