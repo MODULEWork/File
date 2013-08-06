74f2bf02f25a0c6267f34f8a1a009d60bb032d28
ChristianGaertner <christiangaertner.film@googlemail.com>
2013-08-06 17:07:00 +0200

    Adding first files

---
+++ src/Modulework/Modules/File/File.php
@@ -0,0 +1,12 @@
+<?php namespace Modulework\Modules\File;
+/*
+ * (c) Christian Gärtner <christiangaertner.film@googlemail.com>
+ * This file is part of the Modulework Framework
+ * License: View distributed LICENSE file
+ */
+
+/**
+* File
+* This class represents a file.
+*/
+class File { }
---
+++ tests/Modulework/Modules/File/FileTest.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * (c) Christian Gärtner <christiangaertner.film@googlemail.com>
+ * This file is part of the Modulework Framework Tests
+ * License: View distributed LICENSE file
+ *
+ * 
+ * This file is meant to be used in PHPUnit Tests
+ */
+use Modulework\Modules\Http\File;
+/**
+* PHPUnit Test
+*/
+class FileTest extends PHPUnit_Framework_TestCase { }
